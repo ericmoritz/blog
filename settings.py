229616e4da047a8a3f8f401a32bd5ba6be9523af
Eric Moritz <eric@themoritzfamily.com>
2011-10-26 03:24:17 -0400

    Fixed my links

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 AUTHOR = "Eric Moritz"
-SITENAME = "One More Blog"
+SITENAME = "Eric Moritz"
 CATEGORY_FEED_RSS = "feeds/category/%s.rss.xml"
+
+DEFAULT_CATEGORY = "blog"
 
 FEED = "feeds/index.atom.xml"
 FEED_RSS = "feeds/index.rss.xml"
@@ -10,14 +12,21 @@ TAG_FEED_RSS = "feeds/tags/%s.rss.xml"
 
 GOOGLE_ANALYTICS = "UA-75939-1"
 TWITTER_USERNAME = "ericmoritz"
-LINKS =[ 
+
+MENUITEMS =[ 
     ("github", "https://github.com/ericmoritz/"),
     ("gist.github", "https://gist.github.com/ericmoritz/"),
-]
-
-SOCIAL = [
     ("Google+", "https://plus.google.com/111783618530459182533/"),
+
 ]
 
 WITH_PAGINATION = True
 DEFAULT_PAGINATION = 20
+
+THEME = "themes/crisp"
+
+DEFAULT_METADATA = [
+    ("author", "Eric Moritz")
+    ]
+
+GRAVATAR_HASH = "4839d0678248e68eaeed5084e788210b"
